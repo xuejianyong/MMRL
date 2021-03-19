655916de2ce53ca6b42e7c7cc438540568f868af
xuejianyong <xuejianyong@hotmail.com>
2021-03-19 13:27:04 +0100

    version

--- MMRL-architecture codes/RL_brain.py
+++ MMRL-architecture codes/RL_brain.py
@@ -1,9 +1,8 @@
 # -*- coding: UTF-8 -*-
 """
-This part of code is the Q learning brain, which is a brain of the agent.
-All decisions are made in here.
 
-View more on my tutorial page: https://morvanzhou.github.io/tutorials/
+
+
 """
 
 import numpy as np
